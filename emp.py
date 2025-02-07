09b45141a16ded7cbbd25380f79b260fd3186ad7
Dharshan1436 <dharshan1436@gmail.com>
2025-02-07 10:58:18 +0530

    [Dharshan P L] Add : Python Script Refactoring the code using Class Method to Compute Employee Wage

--- emp.py
+++ emp.py
@@ -1,26 +1,27 @@
 import random
-def calculate_wages_until_condition(wage_per_hour):
+class Employee:
+    wage_per_hour = 20
     total_wage = 0
-    working_days = 20
     total_hours = 0
     working_days = 0
-    while total_hours < 100 and working_days < 20:
-        attendance = random.randint(0, 2)
+    @classmethod
+    def calculate_wages_until_condition(cls, max_hours=100, max_days=20):
+        while cls.total_hours < max_hours and cls.working_days < max_days:
+            attendance = random.randint(0, 2)
+            match attendance:
+                case 1:
+                    cls.total_wage += cls.wage_per_hour * 8
+                    cls.total_hours += 8
+                    cls.working_days += 1
+                case 2:
+                    cls.total_wage += cls.wage_per_hour * 4
+                    cls.total_hours += 4
+                    cls.working_days += 1
+                case _:
+                    pass
+        print(f"Total Wage: {cls.total_wage}")
+        print(f"Total Hours Worked: {cls.total_hours}")
+        print(f"Total Days Worked: {cls.working_days}")
 
-        match attendance:
-            case 1:
-                total_wage += wage_per_hour * 8
-                total_hours += 8
-                working_days += 1
-            case 2:
-                total_wage += wage_per_hour * 4
-                total_hours += 4
-                working_days += 1
-            case _:
-                pass
-
-    print(f"Total Monthly Wage: {total_wage}")
-    print(f"Total Wage: {total_wage}")
-    print(f"Total Hours Worked: {total_hours}")
-    print(f"Total Days Worked: {working_days}")
-calculate_wages_until_condition(20)
+employee = Employee()
+employee.calculate_wages_until_condition()
