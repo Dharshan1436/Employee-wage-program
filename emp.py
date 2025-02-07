53d2c3bd8a75d9a8e23a262e58b2ff01ce613ec9
Dharshan1436 <dharshan1436@gmail.com>
2025-02-07 10:52:26 +0530

    [Dharshan P L] Add : Python Script to Calculate Wages till a condition of total working hours or days is reached for a month

--- emp.py
+++ emp.py
@@ -1,16 +1,26 @@
 import random
-
-def calculate_monthly_wage(wage_per_hour):
+def calculate_wages_until_condition(wage_per_hour):
     total_wage = 0
     working_days = 20
-    for _ in range(working_days):
+    total_hours = 0
+    working_days = 0
+    while total_hours < 100 and working_days < 20:
         attendance = random.randint(0, 2)
+
         match attendance:
             case 1:
                 total_wage += wage_per_hour * 8
+                total_hours += 8
+                working_days += 1
             case 2:
                 total_wage += wage_per_hour * 4
+                total_hours += 4
+                working_days += 1
             case _:
-                total_wage += 0
+                pass
+
     print(f"Total Monthly Wage: {total_wage}")
-calculate_monthly_wage(20)
+    print(f"Total Wage: {total_wage}")
+    print(f"Total Hours Worked: {total_hours}")
+    print(f"Total Days Worked: {working_days}")
+calculate_wages_until_condition(20)
